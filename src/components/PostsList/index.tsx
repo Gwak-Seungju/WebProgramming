0052467ae17a7531fe9eb575b71f959f24ebd15d
곽승주 <tmwn1015@naver.com>
2025-06-10 23:07:48 +0900

    fix: 유저 이미지 테두리 수정

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './PostsList.module.scss';
 import type { Post } from '@/utils/post';
 import PersonIcon from '@/assets/person.svg';
@@ -7,13 +8,26 @@ interface PostsListProps {
 }
 
 export default function PostsList({ posts }: PostsListProps) {
+	const navigate = useNavigate();
+
+	const goNeighborBlog = (id: number, name: string) => {
+		navigate(`/blog/${name}/${id}`);
+	};
+
+	const goDetailPost = (userId: number, name: string, postId: number) => {
+		navigate(`/blog/${name}/${userId}/${postId}`);
+	};
+
 	if (!posts || posts.length === 0) return <p>등록된 글이 없습니다.</p>;
 
 	return (
 		<div className={styles.posts}>
 			{posts.map((post) => (
 				<div key={post.post_id} className={styles.post}>
-					<button className={styles.post__info}>
+					<button
+						className={styles.post__info}
+						onClick={() => goNeighborBlog(post.user_id, post.username)}
+					>
 						<div className={styles.personIcon}>
 							<PersonIcon />
 						</div>
@@ -24,8 +38,36 @@ export default function PostsList({ posts }: PostsListProps) {
 							</div>
 						</div>
 					</button>
-					<h3 className={styles.post__title}>{post.title}</h3>
-					<p className={styles.post__content}>{post.content}</p>
+					<h3
+						className={styles.post__title}
+						onClick={() => {
+							goDetailPost(post.user_id, post.username, post.post_id);
+						}}
+						onKeyDown={(e) => {
+							if (e.key === 'Enter' || e.key === ' ') {
+								goDetailPost(post.user_id, post.username, post.post_id);
+							}
+						}}
+						tabIndex={0}
+						role="button"
+					>
+						{post.title}
+					</h3>
+					<p
+						className={styles.post__content}
+						onClick={() => {
+							goDetailPost(post.user_id, post.username, post.post_id);
+						}}
+						onKeyDown={(e) => {
+							if (e.key === 'Enter' || e.key === ' ') {
+								goDetailPost(post.user_id, post.username, post.post_id);
+							}
+						}}
+						tabIndex={0}
+						role="button"
+					>
+						<span className={styles['post__content--r']}>{post.content}</span>
+					</p>
 					<div>
 						<span>공감: {post.likes_count}</span> |{' '}
 						<span>댓글: {post.comments_count}</span>
