65b761a161d68c45d708ffae3b62a5104e1640f8
곽승주 <tmwn1015@naver.com>
2025-06-12 02:08:01 +0900

    feat: 로그인 페이지 UI 구현

--- src/pages/Auth/LoginPage/index.tsx
+++ src/pages/Auth/LoginPage/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useLogin from '../hooks/useLogin';
+import styles from './LoginPage.module.scss';
+import BlindIcon from '@/assets/blind.svg';
+import EyeIcon from '@/assets/eye.svg';
 
 export default function LoginPage() {
 	const navigate = useNavigate();
 	const { mutate: login } = useLogin();
 	const [form, setForm] = useState({ email: '', password: '' });
+	const [showPw, setShowPw] = useState<boolean>(false);
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setForm({ ...form, [e.target.name]: e.target.value });
@@ -16,28 +20,54 @@ export default function LoginPage() {
 		login(form);
 	};
 
+	const toggleShowPw = () => {
+		setShowPw((prev) => !prev);
+	};
+
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="email"
-					name="email"
-					placeholder="이메일"
-					value={form.email}
-					onChange={handleChange}
-					required
-				/>
-				<input
-					type="password"
-					name="password"
-					placeholder="비밀번호"
-					value={form.password}
-					onChange={handleChange}
-					required
-				/>
-				<button type="submit">로그인</button>
+		<div className={styles.container}>
+			<form className={styles.container__form} onSubmit={handleSubmit}>
+				<div className={styles['container__input--field-id']}>
+					<input
+						type="email"
+						name="email"
+						placeholder="이메일을 입력하세요"
+						value={form.email}
+						onChange={handleChange}
+						required
+						className={styles['container__input--text']}
+					/>
+				</div>
+				<div className={styles['container__input--field-pw']}>
+					<input
+						type={showPw ? 'text' : 'password'}
+						name="password"
+						placeholder="비밀번호를 입력하세요"
+						value={form.password}
+						onChange={handleChange}
+						required
+						className={styles['container__input--text']}
+					/>
+					<button
+						type="button"
+						onClick={toggleShowPw}
+						className={styles['container__input--icon']}
+						tabIndex={-1}
+					>
+						{showPw ? <EyeIcon /> : <BlindIcon />}
+					</button>
+				</div>
+				<button type="submit" className={styles.container__button}>
+					로그인
+				</button>
 			</form>
-			<button onClick={() => navigate('/auth/signup')}>회원가입</button>
+			<button
+				type="button"
+				className={styles.container__signup}
+				onClick={() => navigate('/auth/signup')}
+			>
+				<span>비회원이신가요?</span> 회원가입
+			</button>
 		</div>
 	);
 }
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import styles from './Auth.module.scss';
 
 export default function Auth() {
+	const navigate = useNavigate();
 	return (
 		<div className={styles.container}>
-			<h1>G Blog</h1>
+			<button className={styles.logo} onClick={() => navigate('/')}>
+				G Blog
+			</button>
 			<Outlet />
 		</div>
 	);
