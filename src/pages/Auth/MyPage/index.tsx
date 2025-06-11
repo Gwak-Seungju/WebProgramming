e00ccc643d2eb24752b0b5dc0fa7b5689e35778d
곽승주 <tmwn1015@naver.com>
2025-06-12 03:17:43 +0900

    feat: 마이페이지 UI 구현

--- src/pages/Auth/MyPage/index.tsx
+++ src/pages/Auth/MyPage/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useDeleteUser } from '../hooks/useDeleteUser';
 import { useEditProfile } from '../hooks/useEditProfile';
 import { useUserActivity } from '../hooks/useUserActivity';
@@ -6,6 +7,7 @@ import styles from './MyPage.module.scss';
 import useUserInfo from '@/hooks/useUserInfo';
 
 export default function MyPage() {
+	const navigate = useNavigate();
 	const { data: userInfo } = useUserInfo();
 	const userId = userInfo!.user_id;
 	const { data: userActivity } = useUserActivity(userId);
@@ -26,29 +28,51 @@ export default function MyPage() {
 		deleteUser(userId);
 	};
 
+	const goMyBlog = () => {
+		navigate(`/blog/${userInfo?.username}/${userInfo?.user_id}`);
+	};
+
 	return (
 		<div className={styles.container}>
-			<div>
-				<h2>프로필 수정</h2>
-				<div>
-					<div>이름</div>
-					<input type="text" value={username} onChange={handleUsernameChange} />
-					<button onClick={handleEditProfile}>수정하기</button>
+			<div className={styles.profile}>
+				<div className={styles.profile__title}>프로필 수정</div>
+				<div className={styles.profile__name}>
+					<div className={styles['profile__name--text']}>별명</div>
+					<input
+						type="text"
+						value={username}
+						className={styles['profile__name--input']}
+						onChange={handleUsernameChange}
+					/>
+					<button
+						className={styles['profile__name--edit']}
+						onClick={handleEditProfile}
+					>
+						수정
+					</button>
 				</div>
+				<button className={styles.profile__button} onClick={handleDeleteUser}>
+					회원 탈퇴
+				</button>
 			</div>
-			<div>
-				<h2>내 활동</h2>
-				<div>
+			<div className={styles.activity}>
+				<div className={styles.activity__title}>내 활동</div>
+				<div className={styles.activity__post}>
 					<div>내 글수</div>
-					<div>{userActivity!.post_count}개</div>
+					<div>
+						<span>{userActivity!.post_count}</span>개
+					</div>
 				</div>
-				<div>
+				<div className={styles.activity__neighbor}>
 					<div>내 이웃 목록</div>
-					<div>{userActivity!.friend_count}명</div>
+					<div>
+						<span>{userActivity!.friend_count}</span>명
+					</div>
 				</div>
-				<button>내 블로그로 이동</button>
+				<button className={styles.activity__button} onClick={goMyBlog}>
+					내 블로그로 이동
+				</button>
 			</div>
-			<button onClick={handleDeleteUser}>회원 탈퇴</button>
 		</div>
 	);
 }
